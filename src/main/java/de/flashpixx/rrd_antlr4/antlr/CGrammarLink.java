53d6ff4ebe8318d8fad25b3ad74de5083f373d24
Philipp Kraus <philipp.kraus@flashpixx.de>
2016-02-26 08:55:51 +0100

    update html template

---
+++ src/main/java/de/flashpixx/rrd_antlr4/antlr/CGrammarLink.java
@@ -0,0 +1,60 @@
+/**
+ * @cond LICENSE
+ * ######################################################################################
+ * # LGPL License                                                                       #
+ * #                                                                                    #
+ * # This file is part of the RRD-AntLR4                                                #
+ * # Copyright (c) 2016, Philipp Kraus (philipp.kraus@tu-clausthal.de)                  #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU Lesser General Public License as                     #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU Lesser General Public License for more details.                                #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU Lesser General Public License           #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
+ */
+
+package de.flashpixx.rrd_antlr4.antlr;
+
+import java.util.Arrays;
+
+
+/**
+ * link class between two rules
+ */
+public final class CGrammarLink implements IGrammarSimpleElement<String>
+{
+    /**
+     * rule name
+     */
+    private final String m_value;
+
+    /**
+     * ctor
+     *
+     * @param p_value rule name
+     */
+    public CGrammarLink( final String p_value )
+    {
+        m_value = p_value;
+    }
+
+    @Override
+    public final String get()
+    {
+        return m_value;
+    }
+
+    @Override
+    public final boolean isValueAssignableTo( final Class<?>... p_class )
+    {
+        return m_value == null ? true : Arrays.asList( p_class ).stream().map( i -> i.isAssignableFrom( m_value.getClass() ) ).anyMatch( i -> i );
+    }
+}
